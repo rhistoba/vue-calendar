93c750ee84f26e8e3c63628a4ea3beed510aaa7e
rhistoba <fujitakeisound@gmail.com>
2020-02-10 18:35:03 +0900

    remove unnecessary files

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app" class="font-sans max-w-6xl mx-auto px-4">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
